74dfd7a73d89dc6c9c545f67bb8e769718b5f2f0
sjahn <32211334+asj214@users.noreply.github.com>
2020-12-10 18:58:21 +0900

    update

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"중국산 레포라서 믿어도 되는지 모르겠네 ....",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"근데 이거 왜 댓글이 안붙니",'url':"/2018/07/01/welcome.html"}]};
